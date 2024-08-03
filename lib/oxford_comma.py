74a05fcac71d48ebdb550b925743ada09a05d911
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-08-02 19:08:13 -0700

    Complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,17 @@
 def oxford_comma(items):
-    return None
+    # if len(items) == 1:
+    #     return items[0]
+    # elif len(items) == 2:
+    #     return ' and '.join(items)
+    # else:
+    #     last_item = items.pop()
+    #     str = ', '.join(items)
+    #     final = ', and '.join([str, last_item])
+    #     return final
+    if len(items) > 1:
+        items[-1] = 'and ' + items[-1]
+    
+    if len(items) == 2:
+        return ' '.join(items)
+    else:
+        return ', '.join(items)
